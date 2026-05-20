0208070ae4db7a20442481d85bfb6ee11a8c17be
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-05-20 07:36:04 +0200

    Update بنك.js
    Signed-off-by: sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>

--- modules/commands/بنك.js
+++ modules/commands/بنك.js
@@ -29,7 +29,7 @@ module.exports = {
         countDown: 5,
         prefix: true,
         description: 'نظام اقتصادي متكامل بزخرفة المسار الطولي.',
-        category: 'fun',
+        category: 'tools',
         guide: {
             ar: '●───── ⌬ ─────●\n' +
                 '┇ ⦿ ⟬ الأوامـر الـمـتـاحـة ⟭\n' +
